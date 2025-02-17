23400fa91db13e2a2c2e3986e924ce636eb6ce84
HTKNgan007 <nganha2004nh@gmail.com>
2025-02-18 00:51:01 +0700

    Vidu Servlet

---
+++ ViduServlet/src/main/java/vn/nganha/HelloWorld.java
@@ -0,0 +1,34 @@
+package vn.nganha;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public HelloWorld() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter wt = response.getWriter(); //Luồng , trả dữ liệu về cho client
+		wt.println("Hello World, I'm Ngan");
+	}
+
+}
