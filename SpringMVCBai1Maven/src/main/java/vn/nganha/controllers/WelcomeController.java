bd228ae43bffeeafa1dc11a6c0db6dd851f63ce8
HTKNgan007 <nganha2004nh@gmail.com>
2025-03-03 16:25:25 +0700

    lam lai tao maven project springMVC

---
+++ SpringMVCBai1Maven/src/main/java/vn/nganha/controllers/WelcomeController.java
@@ -0,0 +1,14 @@
+package vn.nganha.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+	//URL cho action này tính từ root
+		@RequestMapping("say-hi")
+		public String SayHello() {
+			// code xử lí
+			return "chao"; // view sẽ là file chao
+		}
+}
