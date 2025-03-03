05842bd083b5bb8e35e93153454e8cbf46540fc3
HTKNgan007 <nganha2004nh@gmail.com>
2025-03-03 22:37:46 +0700

    cap nhat

--- SpringMVCBai1Maven/src/main/java/vn/nganha/controllers/WelcomeController.java
+++ SpringMVCBai1Maven/src/main/java/vn/nganha/controllers/WelcomeController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WelcomeController {
 	//URL cho action này tính từ root
-		@RequestMapping("say-hi")
-		public String SayHello() {
+		@RequestMapping("/welcome")
+		public String welcome() {
 			// code xử lí
-			return "chao"; // view sẽ là file chao
+			return "viewWelcome"; // view sẽ là file viewWelcome
 		}
 }
