9e226787d2de5a59b482e2fd24f9eabd88d28c8a
HTKNgan007 <nganha2004nh@gmail.com>
2025-03-04 10:48:42 +0700

    update

--- ChaoSpringBoot/src/main/java/vn/nganha/ChaoSpringBoot/ChaoController.java
+++ ChaoSpringBoot/src/main/java/vn/nganha/ChaoSpringBoot/ChaoController.java
@@ -1,8 +1,8 @@
 package vn.nganha.ChaoSpringBoot;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller  //Bảo rằng đây là 1 lớp controller
 public class ChaoController {
@@ -10,8 +10,9 @@ public class ChaoController {
 	//action method
 	//Url gọi action
 	@GetMapping("/chao")
-	public String xin_Chao(/*Cac tham so*/) {
+	public String xin_Chao(ModelMap model) {
 		//Code xử lí ở đây
+		model.addAttribute("tb", "Xin Chào nhé <3");
 		return "helloView";
 	}
 }
