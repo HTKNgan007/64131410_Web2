7bbf08328fb2bfc06e004cc620517558192fe29e
HTKNgan007 <nganha2004nh@gmail.com>
2025-03-11 10:21:32 +0700

    truyen du lieu danh sach sang view

--- SB_TruyenDuLieuSangView/src/main/java/vn/nganha/SB_TruyenDuLieuSangView/StudentController.java
+++ SB_TruyenDuLieuSangView/src/main/java/vn/nganha/SB_TruyenDuLieuSangView/StudentController.java
@@ -1,5 +1,7 @@
 package vn.nganha.SB_TruyenDuLieuSangView;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.ModelMap;
@@ -12,4 +14,18 @@ public class StudentController {
         model.addAttribute("student", student);
         return "profileView";
     }
+	@GetMapping("/dsSinhVien")
+	public String studentList(ModelMap m) {
+		ArrayList<Student> dsSinhVien = new ArrayList<>();
+		Student student1 = new Student("64131410", "Hà Thị Kiều Ngân", 2004, "Nữ");
+		Student student2 = new Student("64131234", "Nguyễn Triệu Nam", 2003, "Nam");
+		Student student3 = new Student("64131000", "Hồ Quốc Anh", 2002, "Nam");
+		Student student4 = new Student("64137850", "Đào Quý Cường", 2001, "Nam");
+		dsSinhVien.add(student1);
+		dsSinhVien.add(student2);
+		dsSinhVien.add(student3);
+		dsSinhVien.add(student4);
+		m.addAttribute("dsSV", dsSinhVien);
+		return "profileListView";
+	}
 }
