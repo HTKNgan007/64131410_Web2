97dbcc99352fc771d76c2fc01c8bb6af242b083f
HTKNgan007 <nganha2004nh@gmail.com>
2025-03-15 11:07:24 +0700

    Sb_login

---
+++ SB_Login/src/main/java/vn/nganha/SB_Login/controllers/LoginController.java
@@ -0,0 +1,35 @@
+package vn.nganha.SB_Login.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class LoginController {
+	//Hien thi form dang nhap
+	@GetMapping("/login")
+	public String LoginForm(Model model) {
+		model.addAttribute("message", "");
+		return "login";
+	}
+	
+	//Xu ly
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String Login(HttpServletRequest request, Model model) {
+		//Lay du lieu
+		String dn = request.getParameter("id");
+		String mk = request.getParameter("password");
+		
+		if(dn.equals("NganHa") && mk.equals("Ngan710")) {
+			model.addAttribute("message", "Đăng nhập thành công");
+		} else {
+			model.addAttribute("message", "Sai tên đăng nhập hoặc mật khẩu.\n Đăng nhập thất bại!!");
+		}
+		return "login";
+	}
+	
+}
---
+++ SB_Login/src/test/java/vn/nganha/SB_Login/SbLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.nganha.SB_Login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
