26e61c67c97a02e2640072d6bf6fd11ff103324e
HTKNgan007 <nganha2004nh@gmail.com>
2025-03-25 09:40:54 +0700

    push l3

--- SB_TongHopGK/src/main/java/vn/nganha/SB_TongHopGK/controllers/HomeController.java
+++ SB_TongHopGK/src/main/java/vn/nganha/SB_TongHopGK/controllers/HomeController.java
@@ -1,5 +1,23 @@
 package vn.nganha.SB_TongHopGK.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
 
+	@GetMapping("/")
+	public String Home() {
+		return "home";
+	}
+	
+	@GetMapping("/about")
+	public String About() {
+		return "about";
+	}
+	
+	@GetMapping("/studentList")
+	public String List() {
+		return "studentList";
+	}
 }
